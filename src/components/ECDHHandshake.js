32ce0917734696cf0ba69042f4bff5077198003e
Nora Manolescu <ntab@photohikes.com>
2024-10-28 20:37:42 -0500

    created file for ecdh key exchange

---
+++ src/components/ECDHHandshake.js
@@ -0,0 +1,11 @@
+/*Navigation bar for skyShare.
+Description: This is to generate keys using the ECDH key exchange 
+Programmers: Brynn Hare, Micah Borghese, Katelyn Accola, Nora Manolescu, Kyle Johnson
+Date Created: 10/28/2024
+*/
+
+// Access local browser storage (store public key)
+// See if public key exists in local browser storage
+//      If it doesn't, generate and store public key
+//      If it does, set and use existing public key
+// Listen for other public key 
