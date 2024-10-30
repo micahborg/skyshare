dad337c003de1fe51cd5331ddd4a51c7334574cd
Kyle Johnson <kyleljohnson02@gmail.com>
2024-10-29 23:15:20 -0500

    Mobile

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-/*Main page for skyShare.
+/* Main page for skyShare.
 Description: This page displays the home page for skyShare where files can be dropped and a QR code is created. 
 Programmers: Brynn Hare, Micah Borghese, Katelyn Accola, Nora Manolescu, Kyle Johnson
 Date Created: 10/22/2024
 */
 "use client";
-import { Box, Heading, Button, Card } from "@chakra-ui/react";
+import { Box, Heading, Button, Card, useBreakpointValue } from "@chakra-ui/react";
 import NavBar from "@/components/NavBar";
 import FileUploadModal from "@/components/FileUploadModal";
 import QRCodeGenerator from "@/components/QRCodeGenerator";
@@ -12,20 +12,42 @@ import { useDisclosure } from "@chakra-ui/react";
 
 const Home = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  
-  return (
-    <Box align="center">
-      <NavBar />
-      <Heading fontSize="7xl" m={10}>Welcome to skyShare!</Heading>
 
+  // Responsive font sizes and margins
+  const headingSize = useBreakpointValue({ base: "3xl", md: "5xl", lg: "7xl" });
+  const margin = useBreakpointValue({ base: 4, md: 6, lg: 10 });
+  const buttonFontSize = useBreakpointValue({ base: "sm", md: "md", lg: "lg" });
+  const cardWidth = useBreakpointValue({ base: "90%", md: "70%", lg: "50%" });
+  const qrCodeSize = useBreakpointValue({ base: 100, md: 150, lg: 200 });
+
+  return (
+    <Box align="center" padding={margin}>
+      <NavBar />
       
-      <Card m={2}> 
-        <Button colorScheme="skyBlue" onClick={onOpen}>
+      {/* adjusted heading */}
+      <Heading 
+        fontSize={headingSize} 
+        m={margin} 
+        mt={8} // increased top margin to separate from NavBar
+        textAlign="center"
+      >
+        Welcome to skyShare!
+      </Heading>
+
+      <Card m={margin} p={margin} width={cardWidth}>
+        <Button
+          colorScheme="blue"
+          onClick={onOpen}
+          width="100%"
+          fontSize={buttonFontSize}
+        >
           Upload File
         </Button>
-        <QRCodeGenerator link="https://www.google.com" />
-      </Card>
 
+        <Box mt={4} align="center">
+          <QRCodeGenerator link="https://www.google.com" size={qrCodeSize} />
+        </Box>
+      </Card>
 
       <FileUploadModal isOpen={isOpen} onClose={onClose} />
     </Box>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,27 @@
-/*Navigation bar for skyShare.
+/* Navigation bar for skyShare.
 Description: There is a navigation bar at the top of the site so that the user can toggle between the about and home pages.  
 Programmers: Brynn Hare, Micah Borghese, Katelyn Accola, Nora Manolescu, Kyle Johnson
 Date Created: 10/22/2024
 */
-import { Image, Flex, Heading, HStack, Link } from "@chakra-ui/react";
+import { Image, Flex, Heading, HStack, Link, useBreakpointValue } from "@chakra-ui/react";
 
-const NavBar = () => { //creating the navigation bar at the top of the site
+const NavBar = () => { // creating the navigation bar at the top of the site
+    // responsive font size for the headings
+    const headingSize = useBreakpointValue({ base: "md", md: "lg" });
+
     return (
-        <HStack p={7} bg="sunnyYellow.100" color="black"> 
+        <HStack p={4} bg="sunnyYellow.100" color="black"> 
             <Flex ml={5} gap={10} flexDirection="row" w="70%">
-                <Link href="/"><Heading>Home</Heading></Link>
-                <Link href="/about"><Heading>About</Heading></Link>
+                <Link href="/">
+                    <Heading fontSize={headingSize}>Home</Heading>
+                </Link>
+                <Link href="/about">
+                    <Heading fontSize={headingSize}>About</Heading>
+                </Link>
             </Flex>
             <Flex mr={5} w="30%" align="center" flexDirection="row-reverse">
-                <Image boxSize="60px" src="images/giraffetransparent.png" alt="skyShare logo" />
-                <Heading>skyShare</Heading>
+                <Image boxSize="50px" src="images/giraffetransparent.png" alt="skyShare logo" />
+                <Heading fontSize={headingSize}>skyShare</Heading>
             </Flex>
         </HStack>
     );
