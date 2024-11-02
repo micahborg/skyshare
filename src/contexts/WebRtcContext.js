b9df32eedfa1644b7dee62b0bc60adb77e351edf
Micah Borghese <micahborghese@gmail.com>
2024-11-02 17:14:42 -0500

    try different ICE

--- src/contexts/WebRtcContext.js
+++ src/contexts/WebRtcContext.js
@@ -26,10 +26,30 @@ export const WebRtcProvider = ({ children }) => {
     const firestore = getFirestore(app);
 
     const servers = {
+      // iceServers: [
+      //   { urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"] },
+      // ],
+      // iceCandidatePoolSize: 10,
       iceServers: [
-        { urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"] },
+        {
+          urls: "stun:openrelay.metered.ca:80"
+        },
+        {
+          urls: "turn:openrelay.metered.ca:80",
+          username: "openrelayproject",
+          credential: "openrelayproject"
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443",
+          username: "openrelayproject",
+          credential: "openrelayproject"
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443?transport=tcp",
+          username: "openrelayproject",
+          credential: "openrelayproject"
+        }
       ],
-      iceCandidatePoolSize: 10,
     };
 
     pc.current = new RTCPeerConnection(servers);
