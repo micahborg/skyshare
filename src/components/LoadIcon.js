e4985c1d0bf970988e0669e403162b2ae3beb806
brynnhare <brynnhare88@gmail.com>
2024-11-03 19:01:36 -0600

    load icon fix

--- src/components/LoadIcon.js
+++ src/components/LoadIcon.js
@@ -2,15 +2,15 @@
 Description: This displays a moving giraffe anytime there is something on the site that needs to load.  
 Programmers: Brynn Hare
 Date Created: 10/29/2024
+Edit Dates: 11/3
 */
 
 "use client";
 import React from "react"; //import the react module
-import useGif from '../../public/images/transparentgiraffegif.gif' //grab the loading gif from the images folder
 const Load = () => { //create the load component
     return (
         <div style={styles.loads}> 
-            <img src={useGif} alt="Loading..." style={styles.gif} />
+            <img src="/images/transparentgiraffegif.gif" alt={"Loading..."} style={styles.gif} />
             <p>Loading...</p>
         </div>
     )
@@ -25,8 +25,8 @@ const styles = { //style the load and use the correct gif
         height: '100vh'
     },
     gif: {
-        width: '60px',
-        height: '60px',
+        width: '200px',
+        height: '200px',
     },
 };
 
