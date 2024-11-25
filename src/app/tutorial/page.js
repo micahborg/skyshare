c4cc8e08dcd52140b4c9f20c8868d2e210a864ee
Nora Manolescu <ntab@photohikes.com>
2024-11-25 14:34:34 -0600

    tutorial folder

---
+++ src/app/tutorial/page.js
@@ -0,0 +1,16 @@
+/*About page for skyShare.
+Description: This page has the about discription of our site as well as user testimonials.   
+Programmers: Micah Borghese, Katelyn Accola, Brynn Hare, Nora Manolescu, and Kyle Johnson
+Date Created: 10/29/2024
+Edit Dates: 11/17 (Testimonials), 11/20 (About content/mobile resizing)
+*/
+
+"use client";
+import React, { useEffect, useState } from "react";
+import { Link, Heading, Card, Box, useBreakpointValue, Flex, Image, Text } from "@chakra-ui/react";
+import { useLoading } from "@/contexts/LoadingContext";
+import NavBar from "@/components/NavBar";
+import { useDisclosure } from "@chakra-ui/react";
+
+
+export default Tutorial;
