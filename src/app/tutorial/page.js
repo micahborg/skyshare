202ea66a4935531bc70c0ffce17f94497008d037
Nora Manolescu <ntab@photohikes.com>
2024-11-25 14:37:00 -0600

    push

--- src/app/tutorial/page.js
+++ src/app/tutorial/page.js
@@ -4,13 +4,3 @@ Programmers: Micah Borghese, Katelyn Accola, Brynn Hare, Nora Manolescu, and Kyl
 Date Created: 11/25/2024
 Edit Dates: 
 */
-
-"use client";
-import React, { useEffect, useState } from "react";
-import { Link, Heading, Card, Box, useBreakpointValue, Flex, Image, Text } from "@chakra-ui/react";
-import { useLoading } from "@/contexts/LoadingContext";
-import NavBar from "@/components/NavBar";
-import { useDisclosure } from "@chakra-ui/react";
-
-
-export default Tutorial;
