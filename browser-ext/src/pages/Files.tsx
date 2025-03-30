e5f09fea8708775712d2718e85672d27b70a4d0a
kjaccola <katelyn.accola@ku.edu>
2025-03-29 21:40:57 -0400

    file delete option

--- browser-ext/src/pages/Files.tsx
+++ browser-ext/src/pages/Files.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { useState} from 'react';
-import { Box, Textarea, Text, Button, VStack, HStack, Flex, Heading, Input } from "@chakra-ui/react";
+import React, { useState } from 'react';
+import { Box, Text, Button, VStack, HStack, Heading, Input } from "@chakra-ui/react";
 import theme from "../theme";
 
 const Files = () => {
@@ -11,6 +11,12 @@ const Files = () => {
       setUploadedFiles(Array.from(event.target.files));
     }
   };
+
+  const deleteFile = (index: number) => {
+    const updatedFiles = uploadedFiles.filter((_, i) => i !== index);
+    setUploadedFiles(updatedFiles);
+  };
+
   return (
     <VStack
       spacing={4}
@@ -22,7 +28,7 @@ const Files = () => {
       <Heading fontSize="xl" fontWeight="bold" textAlign="center">
         Files
       </Heading>
-      
+
       <HStack spacing={4} alignItems="flex-start" height="100%">
         {/* Button Section */}
         <VStack
@@ -30,14 +36,12 @@ const Files = () => {
           borderRadius="lg"
           boxShadow="md"
           backgroundColor="sunnyYellow.100"
-          // alignItems="stretch"
           alignItems="center"
           justifyContent="center"
           h="100%"
-          width = "47%"
+          width="47%"
         >
-            
-            <Input
+          <Input
             type="file"
             multiple
             onChange={handleFileChange}
@@ -53,7 +57,6 @@ const Files = () => {
           </Button>
           <Button width="100%">Share</Button>
         </VStack>
-            
 
         {/* Uploaded and Received Files Section */}
         <Box
@@ -72,10 +75,28 @@ const Files = () => {
             <Text color="black" fontSize="xl">Uploaded Files</Text>
             {uploadedFiles.length > 0 ? (
               uploadedFiles.map((file, index) => (
-                <Box key={index} p={2} borderWidth={1} borderRadius="md" width="100%">
-                  <Text fontSize="md">{file.name}</Text>
-                  <Text fontSize="sm" color="gray.500">{(file.size / 1024 / 1024).toFixed(2)} MB</Text>
-                </Box>
+                <HStack
+                  key={index}
+                  p={2}
+                  borderWidth={1}
+                  borderRadius="md"
+                  width="100%"
+                  justifyContent="space-between"
+                >
+                  <Box>
+                    <Text fontSize="md">{file.name}</Text>
+                    <Text fontSize="sm" color="gray.500">
+                      {(file.size / 1024 / 1024).toFixed(2)} MB
+                    </Text>
+                  </Box>
+                  <Button
+                    size="sm"
+                    colorScheme="red"
+                    onClick={() => deleteFile(index)}
+                  >
+                    Delete
+                  </Button>
+                </HStack>
               ))
             ) : (
               <Text color="black">No files uploaded yet.</Text>
