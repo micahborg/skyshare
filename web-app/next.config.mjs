c846e164e7e712df60109cbcb086aab8402ab851
Micah Borghese <micahborghese@gmail.com>
2025-05-06 11:32:29 -0500

    chrome redirect

--- web-app/next.config.mjs
+++ web-app/next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/chrome',
+        destination: 'https://chromewebstore.google.com/detail/skyshare-p2p-file-sharing/bamcpkombebdheapgcdeadnnminhicei',
+        permanent: true, // Use true for permanent (308) redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
